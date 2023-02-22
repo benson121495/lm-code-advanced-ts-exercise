20bb7856c8b297fd94e265309446b36193357f4e
Benson <vivionanoha@gmail.com>
2023-02-22 21:41:35 +0000

    change mjs file to ts file

--- client/src/menu/menu.ts
+++ client/src/menu/menu.ts
@@ -1,8 +1,8 @@
-import { states } from "../states/states.mjs";
-import { clear, print, printNewLine, prompt } from "../ui/console.mjs";
+import { states } from "../states/states.js";
+import { clear, print, printNewLine, prompt } from "../ui/console.js";
 
 export async function showMenu() {
-	clear();
+	clear("yes");
 	print("0. Send Server Message", false);
 	print("1. Show all posts", false);
 	print("2. Show all users", false);
---
+++ client/src/states/state.ts
@@ -0,0 +1,13 @@
+import { states } from "./states";
+
+export class State {
+	state = states.MENU;
+
+	get() {
+		return this.state;
+	}
+
+	set(newState : any) {
+		this.state = newState;
+	}
+}
